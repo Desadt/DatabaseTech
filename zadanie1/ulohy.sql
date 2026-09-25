e49c1e4e02770dbc0343e5f0d2835c8b5caff4d2
Desadt <133690660+Desadt@users.noreply.github.com>
2026-09-25 14:15:50 +0200

    uloha 5

--- zadanie1/ulohy.sql
+++ zadanie1/ulohy.sql
@@ -8,6 +8,12 @@ SELECT order_id, c.customer_name, p.category, sales FROM orders
 INNER JOIN customers c ON orders.customer_id = c.customer_id
 INNER JOIN products p ON orders.product_id = p.product_id;
 
+--uloha 4
 SELECT region, SUM(o.sales) AS total_sales from customers
 LEFT JOIN orders o ON customers.customer_id = o.customer_id
 GROUP BY region;
+
+--uloha 5
+SELECT product_name, COALESCE(SUM(o.sales), 0) AS total_sales FROM products
+LEFT JOIN orders o ON products.product_id = o.product_id
+GROUP BY product_name ORDER BY total_sales DESC;
