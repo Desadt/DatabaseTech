c89cd8c8d51b5838c09e0445a00e2b39affc675b
Desadt <133690660+Desadt@users.noreply.github.com>
2026-09-25 14:09:50 +0200

    uloha 4

--- zadanie1/ulohy.sql
+++ zadanie1/ulohy.sql
@@ -7,3 +7,7 @@ WHERE sales > 500 ORDER BY sales DESC;
 SELECT order_id, c.customer_name, p.category, sales FROM orders
 INNER JOIN customers c ON orders.customer_id = c.customer_id
 INNER JOIN products p ON orders.product_id = p.product_id;
+
+SELECT region, SUM(o.sales) AS total_sales from customers
+LEFT JOIN orders o ON customers.customer_id = o.customer_id
+GROUP BY region;
